55e3d23c427a681fc0ea162e98eb5c6a496aee54
Paulo <phsoares.ita@gmail.com>
2016-04-27 09:30:43 -0300

    Avoid mutation of @data from external reference

--- lib/mock_redis/utility_methods.rb
+++ lib/mock_redis/utility_methods.rb
@@ -6,10 +6,17 @@ class MockRedis
       begin
         send(assertion, key)
         data[key] ||= empty_thing_generator.call
-        yield data[key]
+        data_key_ref = data[key]
+        ret = yield data[key]
+        data[key] = data_key_ref if data[key].nil?
+        primitive?(ret) ? ret.dup : ret
       ensure
         clean_up_empties_at(key)
       end
+    end
+
+    def primitive?(value)
+      value.kind_of?(::Array) || value.kind_of?(::Hash) || value.kind_of?(::String)
     end
 
     def clean_up_empties_at(key)
